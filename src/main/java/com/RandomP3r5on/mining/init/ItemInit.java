ab8c63418dbc018ee380ed459edd20b9f7319cf3
RandomP3r5on <67332950+RandomP3r5on@users.noreply.github.com>
2020-07-07 15:39:19 +0100

    Fixed more

--- src/main/java/com/RandomP3r5on/mining/init/ItemInit.java
+++ src/main/java/com/RandomP3r5on/mining/init/ItemInit.java
@@ -2,6 +2,7 @@ package com.RandomP3r5on.mining.init;
 
 import com.RandomP3r5on.mining.Mining;
 import com.RandomP3r5on.mining.objects.items.Coke;
+import net.minecraft.block.Block;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraft.item.crafting.Ingredient;
@@ -10,7 +11,6 @@ import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.fluids.FluidAttributes;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
